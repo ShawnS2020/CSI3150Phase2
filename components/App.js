2d2beb65ea2baefdef3700d36e23adc215e33da5
ShawnS2020 <shawnathon4@gmail.com>
2023-04-26 13:43:04 -0400

    refactored w/o navigation

--- components/App.js
+++ components/App.js
@@ -1,22 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home } from './Home.js';
-import { Generate } from './Generate.js';
-import { Collection } from './Collection.js';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import { useState } from 'react';
+import { SelectList } from './SelectList.js';
 
-const Stack = createNativeStackNavigator();
+let verse;
 
 export default function App() {
 
+	const [verse, setVerse] = useState({content:""});
+	const [collection, setCollection] = useState([]);
+
+	function collectionHandler(newVerse) {
+		const updatedCollection = collection.slice();
+		let doesExist = false;
+		for (let i = 0; i < collection.length; i ++) {
+			if (collection[i].id == newVerse.id) {
+				doesExist = true;
+			}
+		}
+		if (doesExist == false) {
+			updatedCollection.push(newVerse);
+			setCollection(updatedCollection);
+		}
+	}
+
+	function generateVerse() {
+		if (collection.length > 0) {
+			const randIndex = Math.floor(Math.random() * collection.length);
+			setVerse(collection[randIndex]);
+		}
+	}
+
 	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-			        <Stack.Screen name="Home" component={Home}/>
-			        <Stack.Screen name="Generate" component={Generate}/>
-			        <Stack.Screen name="Collection" component={Collection}/>
-			</Stack.Navigator>
-		</NavigationContainer>
+		<View style={styles.container}>
+			<View style={styles.generateWrapper}>
+				<Button
+					style={styles.button}
+					title="Generate a verse"
+					onPress={() => generateVerse()}
+				/>
+				<Text style={styles.verse}>{verse.content}</Text>
+			</View>
+			<View style={styles.yourVersesWrapper}>
+				<Text style={styles.text}>Your verses</Text>
+				<FlatList
+					numColumns={5}
+					style={styles.list}
+					data={collection}
+					renderItem={({item}) => <Text style={styles.listItem}>{item.id}   </Text>}
+				/>
+			</View>
+				<Text style={styles.text}>Add a verse to your collection</Text>
+				<SelectList collectionHandler={collectionHandler}/>
+		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	generateWrapper: {
+		marginTop: 50,
+		width: "100%",
+		backgroundColor: "pink",
+	},
+	verse: {
+		marginVertical: 10,
+		textAlign: "center",
+	},
+	button: {
+		flex: 3,
+		height: 90
+	},
+	text: {
+		textAlign: "center",
+		fontWeight: "bold",
+		fontSize: 20
+	},
+	yourVersesWrapper: {
+		alignItems: "center",
+		height: 150
+	},
+	list: {
+		flexDirection: 'row',
+		flexWrap: 'wrap'
+	},
+	webView: {
+		flex: 3,
+		backgroundColor: "pink"
+	}
+});
--- components/SelectList.js
+++ components/SelectList.js
@@ -1,39 +1,52 @@
 import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const apiKey = "1863e78f66b2c46e9cf864ca79c4ae90";
+// api data
+const apiKey = "81558319b1070f8bb4f17fb32a8d93d0";
 const bibleId = "de4e12af7f28f599-01";
 const url = "https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/books?include-chapters=true";
-const books = [];
-let chapters = [];
-const verses = [];
 
-(async () => {
-	try {
-		const response = await fetch(url, {
-			method: "GET",
-			headers: {
-				Accept: "application/json",
-				"api-key": apiKey
-			}
-		});
-		const body = await response.json();
-		for (let i = 0; i < body.data.length; i ++) {
-			if (i < 39 || i > 52) {
-				const chapterCount = body.data[i].chapters.length - 1;
-				books.push({ title:body.data[i].name, id:body.data[i].id, chapterCount:chapterCount });
-			}
-		}
-	} catch (error) {
-		console.error(error);
+export const SelectList = ({collectionHandler}) => {
+	
+	// State is an object with "depth" that the FlatList is in. I.e. whether it is displaying books, chapters of a book, or verses of a chapter
+	// And an attribute called items which is an array of the items being displayed
+	const [list, setList] = useState("");
+
+	if (list == "") {
+		getBooks();
 	}
-})();
 
-export const SelectList = ({myList}) => {
-	console.log(myList);
+	// Initial api call to get all books
+	async function getBooks() {
+		const books = [];
+		try {
+			const response = await fetch(url, {
+				method: "GET",
+				headers: {
+					Accept: "application/json",
+					"api-key": apiKey
+				}
+			});
+			const body = await response.json();
+			for (let i = 0; i < body.data.length; i ++) {
+				// Ignoring apocryphal books
+				if (i < 39 || i > 52) {
+					const chapterCount = body.data[i].chapters.length - 1;
+					// Book items have a title for displaying in the FlatList
+					// An id needed for getting that books chapters
+					// And a chapter count
+					books.push({ title:body.data[i].name, id:body.data[i].id, chapterCount:chapterCount });
+				}
+			}
+			// list keeps track of the depth, books, chapters, and verses so data persists as the user moves through the list
+			setList({depth:"books", items:books, books:books, chapters:[], verses:[]});
+		} catch (error) {
+			console.error(error);
+		}
+	};
 	
-	const [list, setList] = useState({depth:"books", items:books.slice()});
-	
+	// This function is called when the user clicks a chapter
+	// API gets the amount of verses in the chapter for displaying the FlatList
 	async function getVerseCount(chapterId) {
 		const url = "https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/chapters/" + chapterId + "/verses";
 		try {
@@ -51,35 +64,57 @@ export const SelectList = ({myList}) => {
 		}
 	}
 
-	function increaseDepth(item) {
+	// This function gets a specific verse when the user clicks a verse
+	// Verse is used to update the state in the parent component
+	async function getVerse(verseId) {
+		const url = "https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/verses/" + verseId + "?content-type=text&include-notes=false&include-titles=true&include-chapter-numbers=false&include-verse-numbers=true&include-verse-spans=false&use-org-id=false";
+		try {
+			const response = await fetch(url, {
+				method: "GET",
+				headers: {
+					Accept: "application/json",
+					"api-key": apiKey
+				}
+			});
+			const body = await response.json();
+			return body.data.content;
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	// Function for when the user clicks any button besides back
+	async function increaseDepth(item) {
 		if (list.depth == "books") {
 			chapters = [];
 			for (let i = 1; i <= item.chapterCount; i ++) {
 				chapters.push({ title:"" + i, chapterId:item.id + "." + i});
 			}
-			setList({depth:"chapters", items:chapters});
+			setList({depth:"chapters", items:chapters, books:list.books, chapters:chapters, verses:[]});
 		} else if (list.depth == "chapters") {
 			const verses = [];
-			let verseCount;
-			getVerseCount(item.chapterId).then(value => {
-				for (let i = 0; i < value; i ++) {
-					verses.push({ title:"" + (i + 1) });
-				}
-				setList({depth:"verses", items:verses});
-			});
+			const verseCount = await getVerseCount(item.chapterId);
+			for (let i = 0; i < verseCount; i ++) {
+				verses.push({ title:"" + (i + 1), verseId:item.chapterId + "." + (i + 1)});
+			}
+			setList({depth:"verses", items:verses, books:list.books, chapters:list.chapters, verses:verses});
 		} else if (list.depth == "verses") {
-			
+			const verseId = item.verseId;
+			const verse = {id:verseId, content:await getVerse(verseId)};
+			collectionHandler(verse);
 		}
 	}
 
+	// Function for when the user clicks back
 	function decreaseDepth() {
 		if (list.depth == "chapters") {
-			setList({depth:"books", items:books});
+			setList({depth:"books", items:list.books, books:list.books, chapters:[], verses:[]});
 		} else if (list.depth == "verses") {
-			setList({depth:"chapters", items:chapters});
+			setList({depth:"chapters", items:list.chapters, books:list.books, chapters:list.chapters, verses:[]});
 		}
 	}
 
+	// Back button only appears when depth is at chapters or verses
 	const BackButton = () => {
 		if (list.depth != "books") {
 			return (
@@ -91,6 +126,7 @@ export const SelectList = ({myList}) => {
 		}
 	}
 
+	// Label at the top of FlatList changes depending on depth
 	const MenuLabel = () => {
 		let label;
 		if (list.depth == "books") {
